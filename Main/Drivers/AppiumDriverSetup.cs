111098e3957b8033edb1d18fc9c9f38ab7e5c3cb
ramkumar78 <shriram2222@gmail.com>
2025-03-06 17:05:16 +0000

    Updated AppiumDriverSetup.cs for Android 15 and README with troubleshooting

--- Main/Drivers/AppiumDriverSetup.cs
+++ Main/Drivers/AppiumDriverSetup.cs
@@ -21,13 +21,13 @@ namespace MapsNavigationTestSuite.Main.Drivers
             {
                 var options = new AppiumOptions();
                 options.PlatformName = ConfigurationManager.AppSettings["PlatformName"] ?? "Android";
-                options.PlatformVersion = ConfigurationManager.AppSettings["PlatformVersion"] ?? "13.0";
+                options.PlatformVersion = ConfigurationManager.AppSettings["PlatformVersion"] ?? "15";
                 options.DeviceName = ConfigurationManager.AppSettings["DeviceName"] ?? "Pixel_6";
                 options.AutomationName = ConfigurationManager.AppSettings["AutomationName"] ?? "UiAutomator2";
                 options.AddAdditionalAppiumOption("appPackage", ConfigurationManager.AppSettings["AppPackage"] ?? "com.google.android.apps.maps");
                 options.AddAdditionalAppiumOption("appActivity", ConfigurationManager.AppSettings["AppActivity"] ?? "com.google.android.maps.MapsActivity");
 
-                string appiumServer = ConfigurationManager.AppSettings["AppiumServer"] ?? "http://localhost:4724/wd/hub";
+                string appiumServer = ConfigurationManager.AppSettings["AppiumServer"] ?? "http://localhost:4724/";
                 if (string.IsNullOrEmpty(appiumServer))
                 {
                     throw new InvalidOperationException("AppiumServer URL is not configured in App.config.");
